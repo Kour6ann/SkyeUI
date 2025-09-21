b60145a4b969f548360f0d3d62c93fec843ec1bd
Kour6ann <123142295+Kour6ann@users.noreply.github.com>
2025-09-21 17:50:51 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- init.lua
+local SkyeUI = require(script.lib.SkyeUI)
+local SaveManager = require(script.lib.SaveManager)
+local InterfaceManager = require(script.lib.InterfaceManager)
+
+return {
+    Library = SkyeUI,
+    SaveManager = SaveManager,
+    InterfaceManager = InterfaceManager,
+}
