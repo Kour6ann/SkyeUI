b65456dbb45ab3508e2b024493f42937fc463a7a
Kour6ann <123142295+Kour6ann@users.noreply.github.com>
2025-09-21 17:49:23 +0800

    Create DefaultThemes.lua

---
+++ themes/DefaultThemes.lua
@@ -0,0 +1,21 @@
+-- DefaultThemes.lua
+return {
+    Sky = {
+        Background = Color3.fromRGB(240, 248, 255),
+        TabBackground = Color3.fromRGB(225, 240, 250),
+        SectionBackground = Color3.fromRGB(255, 255, 255),
+        Text = Color3.fromRGB(25, 25, 35),
+        SubText = Color3.fromRGB(100, 125, 150),
+        Accent = Color3.fromRGB(65, 170, 230),
+        InElementBorder = Color3.fromRGB(200, 220, 235),
+        DropdownFrame = Color3.fromRGB(245, 250, 255),
+        DropdownHolder = Color3.fromRGB(255, 255, 255),
+        DropdownBorder = Color3.fromRGB(220, 235, 245),
+        DropdownOption = Color3.fromRGB(245, 250, 255),
+        Keybind = Color3.fromRGB(245, 250, 255),
+        ToggleSlider = Color3.fromRGB(200, 220, 235),
+        ToggleToggled = Color3.fromRGB(255, 255, 255),
+        SliderRail = Color3.fromRGB(225, 235, 245),
+        DialogInput = Color3.fromRGB(240, 248, 255),
+    }
+}
